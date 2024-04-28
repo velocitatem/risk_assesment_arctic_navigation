bff72942cdb339d21b9eae57031b20e4b598a2bf
velocitatem <daniel@alves.world>
2024-04-28 17:44:07 +0200

    trimmed new dsign

--- main.py
+++ main.py
@@ -1,76 +1,224 @@
 import sorobn as hh
+import networkx as nx
+import matplotlib.pyplot as plt
+from pgmpy.models import BayesianNetwork
+from pgmpy.inference import VariableElimination
+import networkx as nx
+import re
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import json
 import streamlit as st
 
-with open('edges.json') as f:
-    edges = json.load(f)
-edges = [tuple([name.replace(' ', '_') for name in edge]) for edge in edges]
-bn = hh.BayesNet(*edges)
+from pgmpy.models import BayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
 
-STATES = {
-    'ice_concentration': ['<50%', '50–70%', '> 70%'],
-    'ice_thickness': ['<40', '40–80', '>80'],
-    'ship_speed': ['<5', '5–8', '>8'],
-    'wave': ['<0.5', '0.5–1.25', '> 1.25'],
-    'wind': ['<5.5', '5.5–7.9', '> 7.9'],
-    'wind_wave_effect': ['Low', 'Possible', 'Probable', 'Critical'],
-    'getting_stuck_in_the_ice': ['Severe', 'Remote', 'Minor'],
-    'ship-ice_collision': ['Remote', 'Possible', 'Probable', 'Critical'],
-    'consequences_of_getting_stuck_in_the_ice': ['Minor', 'Major'],
-    'ship_ice_collision_consequences': ['Major', 'Critical'],
-    'risk_of_getting_stuck_in_the_ice': ['Low', 'Medium', 'High'],
-    'ship_ice_collision_risk': ['Low', 'Medium', 'High'],
+# Define the structure of the Bayesian Network
+model = BayesianNetwork([
+    ('wind', 'wind_wave_effect'),
+    ('wave', 'wind_wave_effect'),
+    ('wind', 'wave'),
+    ('wind_wave_effect', 'ice_thickness'),
+    ('wind_wave_effect', 'ice_concentration'),
+    ('ice_thickness', 'ship_speed'),
+    ('ice_thickness', 'getting_stuck_in_the_ice'),
+    ('ice_concentration', 'ship_speed'),
+    ('ice_concentration', 'ship-ice_collision'),
+    ('ship_speed', 'getting_stuck_in_the_ice'),
+    ('ship_speed', 'ship-ice_collision'),
+    ('getting_stuck_in_the_ice', 'composite_risk'),
+    ('ship-ice_collision', 'composite_risk')
+])
+
+states = {
+    'ice_concentration': ['<50%', '50-70%', '>70%'],
+    'ice_thickness': ['<40', '40-80', '>80'],
+    'ship_speed': ['<5', '5-8', '8-11', '>11'],
+    'wave': ['<0.5', '0.5-1.25', '>1.25'],
+    'wind': ['<5.5', '5.5-7.9', '>7.9'],
+    'wind_wave_effect': ['Severe', 'Low'],
+    'getting_stuck_in_the_ice': ['Remote', 'Possible', 'Probable'],
+    'ship-ice_collision': ['Remote', 'Possible', 'Probable'],
     'composite_risk': ['Low', 'Medium', 'High']
 }
 
 
+def extract_values(cpt):
+    """
+    Handle extracting values from a pandas DataFrame, some rows might be first 3 rows of just titles
+    WE need extract values in row so [row,row,row], values will only be digits of 0-1 in format X.X
+    """
+    values = []
+    for row in cpt.iterrows():
+        # all after first col in row
+        val = row[1][1:]
+        val = [str(v) for v in val]
+        # try match all X.X format, check that all values are in X.X format can alos be just 0
+        if not all(re.match(r'\d\.\d|0', v) for v in val):
+            continue
+        values.append([float(v) for v in val])
+    return values
+
+
+SOURCE = "cpts.xlsx"# :%s/\'cpts.xlsx\'/SOURCE/g
+
+
+cpd_wind = TabularCPD(variable='wind', variable_card=3,
+                        values=[[0.5], [0.3], [0.2]],
+                        state_names={'wind': states['wind']}
+                      )  # Probabilities for wind being in the three states
+
+
+
+
+cpt = pd.read_excel(SOURCE, sheet_name='wave')
+wave_values = extract_values(cpt)
+
+cpd_wave = TabularCPD(variable='wave', variable_card=3,
+                        evidence=['wind'],
+                        evidence_card=[3],
+                        values=wave_values,
+                        state_names={'wind': states['wind'],
+                                     'wave': states['wave']}
+                    )
+
+
+
+
+wind_wave_effect = pd.read_excel(SOURCE, sheet_name='wind_wave_effect')
+wind_wave_effect_values = extract_values(wind_wave_effect)
+
+# Define the CPT for 'wind_wave_effect', depending on 'wind' and 'wave'
+cpd_wind_wave_effect = TabularCPD(variable='wind_wave_effect', variable_card=2,
+                                    values=wind_wave_effect_values,
+                                    evidence=['wind', 'wave'],
+                                    evidence_card=[3, 3],
+                                    state_names={'wind': states['wind'],
+                                                 'wave': states['wave'],
+                                                 'wind_wave_effect': states['wind_wave_effect']}
+                                  )
+
+
+ice_thickness = pd.read_excel(SOURCE, sheet_name='ice_thickness')
+ice_thickness_values = extract_values(ice_thickness)
+
+# Define the CPT for 'ice_thickness', depending on 'wind_wave_effect'
+cpd_ice_thickness = TabularCPD(variable='ice_thickness', variable_card=3,
+                                values=ice_thickness_values,
+                                evidence=['wind_wave_effect'],
+                                evidence_card=[2],
+                                state_names={'wind_wave_effect': states['wind_wave_effect'],
+                                            'ice_thickness': states['ice_thickness']}
+                                )
+
+
+
+ice_concentration = pd.read_excel(SOURCE, sheet_name='ice_concentration')
+ice_concentration_values = extract_values(ice_concentration)
+
+# Define the CPT for 'ice_concentration', depending on 'wind_wave_effect'
+cpd_ice_concentration = TabularCPD(variable='ice_concentration', variable_card=3,
+                                    values=ice_concentration_values,
+                                    evidence=['wind_wave_effect'],
+                                    evidence_card=[2],
+                                    state_names={'wind_wave_effect': states['wind_wave_effect'],
+                                                'ice_concentration': states['ice_concentration']}
+                                    )
+
+
+ship_speed = pd.read_excel(SOURCE, sheet_name='ship_speed')
+ship_speed_values = extract_values(ship_speed)
+
+# Define the CPT for 'ship_speed', depending on 'ice_thickness' and 'ice_concentration'
+cpd_ship_speed = TabularCPD(variable='ship_speed', variable_card=4,
+                                values=ship_speed_values,
+                                evidence=['ice_thickness', 'ice_concentration'],
+                                evidence_card=[3, 3],
+                                state_names={'ice_thickness': states['ice_thickness'],
+                                            'ice_concentration': states['ice_concentration'],
+                                            'ship_speed': states['ship_speed']}
+                                )
+
+
+getting_stuck = pd.read_excel(SOURCE, sheet_name='getting_stuck')
+getting_stuck_values = extract_values(getting_stuck)
+
+# Define the CPT for 'getting_stuck_in_the_ice', depending on 'ship_speed'
+cpd_getting_stuck = TabularCPD(variable='getting_stuck_in_the_ice', variable_card=3,
+                                values=getting_stuck_values,
+                                evidence=['ship_speed', 'ice_thickness'],
+                                evidence_card=[4, 3],
+                                state_names={'ship_speed': states['ship_speed'],
+                                            'ice_thickness': states['ice_thickness'],
+                                            'getting_stuck_in_the_ice': states['getting_stuck_in_the_ice']}
+                                 )
+
+
+ship_ice_collision = pd.read_excel(SOURCE, sheet_name='ship_ice_collision')
+ship_ice_collision_values = extract_values(ship_ice_collision)
+
+
+# Define the CPT for 'ship-ice_collision', depending on 'ship_speed'
+cpd_ship_ice_collision = TabularCPD(variable='ship-ice_collision', variable_card=3,
+                                    values=ship_ice_collision_values,
+                                    evidence=['ship_speed', 'ice_concentration'],
+                                    evidence_card=[4,3],
+                                    state_names={'ship_speed': states['ship_speed'],
+                                                'ice_concentration': states['ice_concentration'],
+                                                'ship-ice_collision': states['ship-ice_collision']}
+                                    )
+
+
+composite_risk = pd.read_excel(SOURCE, sheet_name='composite_risk')
+composite_risk_values = extract_values(composite_risk)
+
+# Define the CPT for 'composite_risk', depending on 'getting_stuck_in_the_ice' and 'ship-ice_collision'
+cpd_composite_risk = TabularCPD(variable='composite_risk', variable_card=3,
+                                values=composite_risk_values,
+                                evidence=['getting_stuck_in_the_ice', 'ship-ice_collision'],
+                                evidence_card=[3, 3],
+                                state_names={'getting_stuck_in_the_ice': states['getting_stuck_in_the_ice'],
+                                            'ship-ice_collision': states['ship-ice_collision'],
+                                            'composite_risk': states['composite_risk']}
+                                )
+
+
+
+cpts = [cpd_wind, cpd_wave, cpd_wind_wave_effect, cpd_ice_thickness, cpd_ice_concentration, cpd_ship_speed, cpd_getting_stuck, cpd_ship_ice_collision, cpd_composite_risk]
+model.add_cpds(*cpts)
+
+
+
+# Validate the model to ensure the CPTs are correctly defined and compatible
+assert model.check_model()
+
+
+inference = VariableElimination(model)
+
+G = nx.DiGraph()
+G.add_nodes_from(model.nodes())
+G.add_edges_from(model.edges())
+
 @st.cache_resource
-def get_samples():
-    samples = []
-    for i in range(500):
-        sample = {key: np.random.choice(STATES[key]) for key in STATES.keys()}
-        samples.append(sample)
-    return samples
+def get_gr():
+    pos = nx.spring_layout(G)  # positions for all nodes
+    nx.draw(G, pos, with_labels=True, node_size=2000, node_color="skyblue", font_size=10, font_weight='bold')
+    plt.title('Bayesian Network Graph')
+    plt.savefig('graph.png')
 
-samples = get_samples()
-bn.fit(pd.DataFrame(samples))
+    st.title('Bayesian Network')
 
+    st.write('This is a simple example of a Bayesian Network. The nodes represent different events and the edges represent the dependencies between them.')
 
+    return 'graph.png'
 
-print(samples)
+st.image(get_gr())
 
-bn.prepare()
+wind = st.selectbox('Wind', states['wind'])
+wave = st.selectbox('Wave', states['wave'])
+result = inference.query(variables=['composite_risk'], evidence={'wind': wind, 'wave': wave})
+max_index = np.argmax(result.values)
+mxvar = result.state_names['composite_risk'][max_index]
 
-
-@st.cache_resource
-def get_plot():
-    dot = bn.graphviz()
-    path = dot.render('graph', format='png', cleanup=True)
-    return path
-
-st.title('Bayesian Network')
-
-st.write('This is a simple example of a Bayesian Network. The nodes represent different events and the edges represent the dependencies between them.')
-
-st.image(get_plot())
-
-# let user input causes wind, wave
-wind = st.selectbox('Wind speed', STATES['wind'])
-wave = st.selectbox('Wave height', STATES['wave'])
-
-# calculate the probability of getting stuck in the ice
-p = bn.query('composite_risk', event={'wind': wind, 'wave': wave})
-# get argmax
-risk = p.idxmax()
-st.write(f'## The risk of getting stuck in the ice is {risk}')
-
-
-
-st.write('The nodes and states distribution are as follows:')
-# for each node, show the states
-for node in bn.nodes:
-    st.write(f'## {node}')
-    st.write(bn.P[node])
+st.write(f'Composite Risk: {mxvar}')
